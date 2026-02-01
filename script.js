b40e79a5d463d68e02c5c4d4bb11697a6564b0d9
Ansh Gangwar <anshgangwar998@gmail.com>
2026-02-01 23:06:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+    let r = parseFloat(document.getElementById("radius").value);
+
+    if (isNaN(r) || r < 0) {
+        document.getElementById("volume").value = "";
+        return false;
+    }
+
+    let v = (4 / 3) * Math.PI * Math.pow(r, 3);
+    document.getElementById("volume").value = v.toFixed(4);
+
+    return false;
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
